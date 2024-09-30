6f7fed08ce87e750eb5dfcb17d2fb4374f6c1cc8
Somiyo <somiyomagar@gmail.com>
2024-09-29 19:52:31 -0600

    added author, time and used badges for sources

--- src/components/News.js
+++ src/components/News.js
@@ -85,6 +85,9 @@ export class News extends Component {
                   description={element.description ? element.description.slice(0, 88) : ''}
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
                 />
               </div>
             )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props
     return (
       <div className="my-3">
-        <div className="card"  >
+        <div className="card">
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>
+            {source}
+          </span>
           <img
             src={
               !imageUrl
@@ -18,8 +22,17 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}...</p>
+            <p class="card-text">
+              <small class="text-body-secondary">
+                By {!author ? 'unknown' : author} on{' '}
+                {new Date(date).toLocaleString('en-US', {
+                  timeZone: 'America/Denver',
+                })}{' '}
+                MDT
+              </small>
+            </p>
             <a
-              rel='noreferrer'
+              rel="noreferrer"
               href={newsUrl}
               target=" _blank"
               className="btn btn-sm btn-dark"
