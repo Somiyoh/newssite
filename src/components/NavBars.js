4695a58d2d7f8445471288c1a558838cfe3c8b30
Somiyo <somiyomagar@gmail.com>
2024-10-01 14:48:51 -0600

    added stable navbar and used hooks

--- src/components/NavBars.js
+++ src/components/NavBars.js
@@ -4,7 +4,7 @@ import React from 'react'
 const NavBars = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             QuickNews
--- src/components/News.js
+++ src/components/News.js
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
-const News = (props) => {
+const News = ({
+  country = 'us',
+  pageSize = 5,
+  category = 'general',
+  apiKey,
+  setProgress,
+}) => {
   const [articles, setArticles] = useState([])
   const [loading, setLoading] = useState(true)
   const [page, setPage] = useState(1)
   const [totalResults, setTotalResults] = useState(0)
-  // document.title = `${capitalizeFirstLetter(
-  //   props.category
-  // )} - QuickNews`
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
 
-  const updateNews = async () => {
-    props.setProgress(10)
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
+  const updateNews = useCallback(async () => {
+    setProgress(10)
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`
     setLoading(true)
     let data = await fetch(url)
-    props.setProgress(30)
+    setProgress(30)
     let parsedData = await data.json()
-    props.setProgress(70)
+    setProgress(70)
     setArticles(parsedData.articles)
     setTotalResults(parsedData.totalResults)
     setLoading(false)
-    props.setProgress(100)
-  }
+    setProgress(100)
+  }, [country, category, apiKey, page, pageSize, setProgress]) // Add necessary dependencies
 
   useEffect(() => {
-    updateNews()
-  }, [])
+    document.title = `${capitalizeFirstLetter(category)} - QuickNews`;
+    updateNews();
+    // eslint-disable-next-line
+  }, [category, updateNews])
 
   const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${
+      page + 1
+    }&pageSize=${pageSize}`
     setPage(page + 1)
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
     let data = await fetch(url)
     let parsedData = await data.json()
     setArticles(articles.concat(parsedData.articles))
@@ -46,8 +53,11 @@ const News = (props) => {
 
   return (
     <>
-      <h1 className="text-center" style={{ margin: '40px 0px' }}>
-        QuickNews - Top {capitalizeFirstLetter(props.category)} Headlines
+      <h1
+        className="text-center"
+        style={{ margin: '40px 0px', marginTop: '90px' }}
+      >
+        QuickNews - Top {capitalizeFirstLetter(category)} Headlines
       </h1>
       {loading && <Spinner />}
 
@@ -81,45 +91,16 @@ const News = (props) => {
           </div>
         </div>
       </InfiniteScroll>
-
-      {/* <div
-          className="container d-flex justify-content-between "
-          style={{ marginBottom: '1rem' }}
-        >
-          <button
-            disabled={page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={handlePrevClick}
-          >
-            &larr; Previous
-          </button>
-          <button
-            disabled={
-              page + 1 >
-              Math.ceil(totalResults / props.pageSize)
-            }
-            type="button"
-            className="btn btn-dark"
-            onClick={handleNextClick}
-          >
-            Next &rarr;
-          </button>
-        </div> */}
     </>
   )
-}
-
-News.defaultProps = {
-  country: 'us',
-  pageSize: 5,
-  category: 'general',
 }
 
 News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
   category: PropTypes.string,
+  apiKey: PropTypes.string.isRequired,
+  setProgress: PropTypes.func.isRequired,
 }
 
 export default News
